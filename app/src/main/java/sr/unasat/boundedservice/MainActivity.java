cbcf46bf79230f13ab81d9867db5682bbc934032
mnarain <m.narain@unasat.sr>
2018-02-10 12:24:24 -0300

    Initial commit

---
+++ app/src/main/java/sr/unasat/boundedservice/MainActivity.java
@@ -0,0 +1,41 @@
+package sr.unasat.boundedservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //button objects
+    private Button buttonStart;
+    private Button buttonStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting buttons from xml
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        //attaching onclicklistener to buttons
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonStart) {
+            //start the service here
+            startService(new Intent(this, MediaPlayerService.class));
+        } else if (view == buttonStop) {
+            //stop the service here
+            stopService(new Intent(this, MediaPlayerService.class));
+        }
+    }
+
+}
